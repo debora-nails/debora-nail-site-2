394ccb4e1e75b1f3da0bfad1b854f3a6b2c8ed4e
debora-nails <ds105292@gmail.com>
2026-09-04 22:12:38 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // A chave abaixo é a chave PUBLICÁVEL (Publishable key). Nunca coloque aqui uma service_role/secret key.
 window.SUPABASE_CONFIG = {
   url: 'https://pkkylsrcmhznodcdhrqk.supabase.co',
-  publishableKey: 'COLE_AQUI_SUA_CHAVE_PUBLICAVEL'
+  publishableKey: 'sb_publishable_xobGJgHlxRbC5168HJB_NA_ebWYEnRd'
 };
